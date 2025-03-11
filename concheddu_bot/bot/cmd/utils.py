8ae925a03a671508be12bcaa6afce1e01dc74359
crivella <davidecrivella@yahoo.it>
2025-03-12 00:05:50 +0100

    Also dots

--- concheddu_bot/bot/cmd/utils.py
+++ concheddu_bot/bot/cmd/utils.py
@@ -16,7 +16,7 @@ logger = logging.getLogger('bot')
 
 def sanitize_ffmpeg_filter(afilt: str):
     """Sanitize the ffmpeg filter"""
-    rgx = re.compile(r'^[a-z0-9=_:,\-]+$')
+    rgx = re.compile(r'^[a-z0-9=_:,\-\.]+$')
     res = afilt
     res = res.replace(';', '')
     res = res.replace('|', '')
