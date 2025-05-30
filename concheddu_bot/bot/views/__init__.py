6304b445141d6da5223451b25e6040a8bb2aff49
crivella <davidecrivella@yahoo.it>
2025-05-30 23:47:10 +0200

    Temporary revert

--- concheddu_bot/bot/views/__init__.py
+++ concheddu_bot/bot/views/__init__.py
@@ -1,11 +1,13 @@
 from .list_songs import SongList
 from .playlist import DeletePlaylist, EditPlaylist
-from .quiz import QuizGuessCharacterRunner, QuizHighLowRunner, QuizSongs
+# from .quiz import QuizGuessCharacterRunner, QuizHighLowRunner, QuizSongs
+from .quiz import QuizHighLowRunner, QuizSongs
 from .quiz.quiz_songs import QuizSongsList
 
 __all__ = [
     'SongList',
     'CreatePlaylist', 'DeletePlaylist', 'EditPlaylist',
     'QuizSongs', 'QuizSongsList',
-    'QuizHighLowRunner', 'QuizGuessCharacterRunner'
+    'QuizHighLowRunner',
+    # 'QuizGuessCharacterRunner'
 ]
